bbc333303bd9007e2d71cb852410329b77995657
Tirtho Ray <tirthoray10@gmail.com>
2025-04-14 10:17:05 +0600

    Primary setup done

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='mt-40 text-5xl'>
+           Thi is footer  
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../MainLayout/Layout";
+import Home from "../Pages/Home/Home";
+import NotFound from "../Pages/NotFound/NotFound";
+
+export const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element:<Home />,
+      },
+      { path: "*", element: <NotFound /> }
+    ],
+  },
+]);
