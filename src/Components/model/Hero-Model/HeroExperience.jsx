7b661cb7ebbd67ffc0cdd81ecb1022f761d71cba
Tirtho Ray <tirthoray10@gmail.com>
2025-05-02 16:32:46 +0600

    add partical

--- src/Components/model/Hero-Model/HeroExperience.jsx
+++ src/Components/model/Hero-Model/HeroExperience.jsx
@@ -4,36 +4,39 @@ import React from "react";
 import { useMediaQuery } from "react-responsive";
 import { Room } from "./RoomModel";
 import HeroLight from "./HeroLight";
+import Particles from "./Partical";
 
 const HeroExperience = () => {
   const isTablet = useMediaQuery({ query: "(max-width:1024px)" });
   const isMobile = useMediaQuery({ query: "(max-width:768px)" });
 
   return (
-    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}
-    shadows
-    gl={{ antialias: true }}
-    
+    <Canvas
+      camera={{ position: [0, 0, 15], fov: 45 }}
+      shadows
+      gl={{ antialias: true }}
     >
-           <ambientLight intensity={1} />
-     
+      <ambientLight intensity={1} />
+
       <OrbitControls
         enablePan={false}
         enableZoom={!isTablet}
-        minDistance={20}
+        minDistance={18}
         maxDistance={30}
         minPolarAngle={Math.PI / 10}
-        maxPolarAngle={Math.PI / 1}
+        maxPolarAngle={Math.PI / 0}
       />
 
-
       <HeroLight />
+      <Particles count={100} />
       <group
         scale={isMobile ? 0.6 : isTablet ? 0.8 : 1}
-        position={isMobile ? [-0.80, -1.9, -0.8] : isTablet ? [0,-2,0] : [0, -2.5, 0]}
+        position={
+          isMobile ? [-0.8, -1.9, -0.8] : isTablet ? [0, -2, 0] : [0, -2.5, 0]
+        }
         rotation={[0, -Math.PI / -13, 0]}
       >
-         {/* position={[0,-0.35,0]} */}
+        {/* position={[0,-0.35,0]} */}
         <Room />
       </group>
     </Canvas>
---
+++ src/Components/model/Hero-Model/Partical.jsx
@@ -0,0 +1,67 @@
+import { useRef, useMemo } from "react";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+
+const Particles = ({ count = 100 }) => {
+  const mesh = useRef(null);
+
+  const particles = useMemo(() => {
+    const data = [];
+    for (let i = 0; i < count; i++) {
+      data.push({
+        x: (Math.random() - 0.5) * 15,
+        y: Math.random() * 15,
+        z: (Math.random() - 0.5) * 15,
+        speed: 0.002 + Math.random() * 0.01,
+      });
+    }
+    return data;
+  }, [count]);
+
+  const positions = useMemo(() => {
+    const arr = new Float32Array(count * 3);
+    particles.forEach((p, i) => {
+      arr[i * 3] = p.x;
+      arr[i * 3 + 1] = p.y;
+      arr[i * 3 + 2] = p.z;
+    });
+    return arr;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [particles]);
+
+  useFrame(() => {
+    if (!mesh.current) return;
+    const pos = mesh.current.geometry.attributes.position.array;
+    for (let i = 0; i < count; i++) {
+      pos[i * 3 + 1] -= particles[i].speed;
+
+      if (pos[i * 3 + 1] < -5) {
+        pos[i * 3 + 1] = 10 + Math.random() * 5;
+      }
+    }
+    mesh.current.geometry.attributes.position.needsUpdate = true;
+  });
+
+  return (
+    <points ref={mesh}>
+      <bufferGeometry>
+        <bufferAttribute
+          attach="attributes-position"
+          array={positions}
+          count={count}
+          itemSize={3}
+        />
+      </bufferGeometry>
+      <pointsMaterial
+        color="#ffffff"
+        size={0.10} // much bigger particle
+        sizeAttenuation
+        transparent
+        opacity={0.7}
+        depthWrite={false}
+      />
+    </points>
+  );
+};
+
+export default Particles;
